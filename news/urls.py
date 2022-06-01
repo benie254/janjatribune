2f586ee96936471d231e1091599d905f6c9dcf81
Benie <davinci.monalissa@gmail.com>
2022-06-01 06:16:30 +0300

    create a urlconf for new articles

--- news/urls.py
+++ news/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    re_path(r'^$',views.news_of_day,name='newsToday'),
+    path('',views.news_of_day,name='newsToday'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name='pastNews'),
-    re_path(r'^search/',views.search_results,name='search_results'),
-    re_path(r'^article/(\d+)',views.article,name='article')
+    path('search/',views.search_results,name='search_results'),
+    re_path(r'^article/(\d+)$',views.article,name='article'),
+    path('news/article',views.new_article,name='newArticle')
+
 ]
 
 if settings.DEBUG:
