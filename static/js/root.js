fcf3b445a6d3237b4d5cf04d0e5b63f7980f34e6
Benie <davinci.monalissa@gmail.com>
2022-06-10 20:03:41 +0300

    update the root js file--target the newsletter form && create AJAX function. replace url as the conf recently created && indicate the type of request--POST--&&pass the serialize function--it converts the form values into JSON

--- static/js/root.js
+++ static/js/root.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
     $('form').submit(function(event){
         event.preventDefault()
+        form = $("form")
+
+        $.ajax({
+        'url':'/ajax/newsletter/',
+        'type':'POST',
+        'data':form.serialize(),
+        'dataType':'json',
+        'success':function(data){
+        alert(data['success'])
+        },
+        }) // end of Ajax method
+        $('#id_your_name').val('')
+        $('#id_email').val('')
     }) // end of submit event
 })  // end of document ready function
