2e34d7bf9bcd82b9afe4cd1c5271811babf055fb
Benie <davinci.monalissa@gmail.com>
2022-06-11 07:52:47 +0300

    create a put function to handle object updates

--- news/views.py
+++ news/views.py
@@ -77,6 +77,15 @@ class MerchDescription(APIView):
         serializers = MerchSerializer(merch)
         return Response(serializers.data)
 
+    def put(self,request,pk,format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch,request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
 
 def past_days_news(request,past_date):
 
